31e29296582f48221ab19565e84c0073ea82e3a2
Pawel Gurlo <pawel.gurlo@gmail.com>
2025-12-21 17:03:20 +0100

    Add data uploads

--- routers/viewRouter.js
+++ routers/viewRouter.js
@@ -1,5 +1,8 @@
 const {Router} = require("express");
+const multer = require("multer");
 const viewRouter = Router();
+
+const upload = multer({dest: "./public/data/uploads/"})
 
 viewRouter.get("/", (req, res) => {
     res.redirect("/upload");
@@ -7,6 +10,12 @@ viewRouter.get("/", (req, res) => {
 
 viewRouter.get("/upload", (req, res) => {
     res.render("index", {subpage: "upload", title: "Upload", user: req.user, subargs: {}});
+});
+
+// upload files
+viewRouter.post("/upload", upload.single("file"), (req, res) => {
+    console.log("File: ", req.file, "\n", "Other: ", req.body);
+    res.redirect("/upload");
 });
 
 module.exports = {viewRouter};
